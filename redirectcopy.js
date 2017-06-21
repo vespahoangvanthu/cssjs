1e77b4d8d5d679501e773e4b3ae3740088133c58
ngocchungvespa <ngocchung.cms.co@gmail.com>
2017-06-21 19:20:16 +0700

    Add files via upload

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>BẠN MUỐN COPY BÀI VIẾT THÌ VÀO ĐÂY : <a href='http://vespahcm.net'>http://vespahcm.net"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>BẠN MUỐN COPY Ư - ĐÂU CÓ DỄ : <a href='http://vespahcm.net'>http://vespahcm.net"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
